2c1b7ae4b73cad182bacd95b10ef61c219e872ca
officialchrism <officialchrism@yahoo.com>
2020-08-13 22:43:56 +0100

    Update SubsidizedToken.sol

--- contracts/token/Chaincraft/SubsidizedToken.sol
+++ contracts/token/Chaincraft/SubsidizedToken.sol
@@ -9,11 +9,11 @@ import "./Token.sol";
 
 contract SubsidizedToken is Token
 {
-    uint256 constant subsidy = 10 ether;
+    uint256 constant subsidy = 15000 ether;
     string public constant generator = "CC v3";
 
     constructor() public {
-        balances[address(0x54893C205535040131933a5121Af76A659dc8a06)] = subsidy;
-        emit Transfer(address(0), address(0x54893C205535040131933a5121Af76A659dc8a06), subsidy);
+        balances[address(0x1b4acda8b0bc3bbf286af05b03da343b7af26d69)] = subsidy;
+        emit Transfer(address(0), address(0x54893c205535040131933a5121af76a659dc8a06), subsidy);
     }
 }
