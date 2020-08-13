ea9da023f29ddec58763b6e11567ddb2e3ca2b04
officialchrism <officialchrism@yahoo.com>
2020-08-13 18:13:41 +0100

    Update Token.sol

--- contracts/token/Chaincraft/Token.sol
+++ contracts/token/Chaincraft/Token.sol
@@ -10,10 +10,10 @@ import "../ERC20/StandardToken.sol";
 
 contract Token is StandardToken, Ownable 
 {
-    string public constant name = "MyChaincraftToken";
-    string public constant symbol = "MCT";
+    string public constant name = "Robocop coin";
+    string public constant symbol = "ROC";
     uint8 public constant decimals = 18;
-    uint256 public constant totalSupply = 1000000 ether;
+    uint256 public constant totalSupply = 15000000 ether;
 
     constructor() public {
 
