9035acef41596a27beb8bcde5e961227d720db53
officialchrism <officialchrism@yahoo.com>
2020-08-14 02:20:57 +0100

    Update Token.sol

--- contracts/token/Chaincraft/Token.sol
+++ contracts/token/Chaincraft/Token.sol
@@ -13,7 +13,7 @@ contract Token is StandardToken, Ownable
     string public constant name = "Robocop coin";
     string public constant symbol = "ROC";
     uint8 public constant decimals = 18;
-    uint256 public constant totalSupply = 15000000 ether;
+    uint256 public constant totalSupply = 150000000000000000000000 ether;
 
     constructor() public {
 
